98831f11dcde9bec4325f6d313ebd1eacf96ec5e
Lucivaldo Castro <lucivaldocb@gmail.com>
2022-08-14 00:06:28 -0300

    Exemplo de autenticação utilizando Redux funcionando adequadamente

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App';
+
+import { store } from './store';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,6 +15,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 );
